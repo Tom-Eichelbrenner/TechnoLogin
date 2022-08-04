efeaaffbc95807b837de0391a7010af1200ff8cb
Tom-Eichelbrenner <tom.eichelbrenner01@gmail.com>
2022-08-04 12:35:41 +0200

    :heavy_plus_sign: add bulma

--- webpack.config.js
+++ webpack.config.js
@@ -21,9 +21,10 @@ Encore
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
     .addEntry('app', './assets/js/app.js')
+    .addStyleEntry('Bulma', './assets/css/app.scss')
 
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
-    .splitEntryChunks()
+    // .splitEntryChunks()
 
     // will require an extra script tag for runtime.js
     // but, you probably want this, unless you're building a single-page app
