ad0bd84adac1f7f157372da6f455e4a99b7c5e1b
Tom-Eichelbrenner <tom.eichelbrenner01@gmail.com>
2022-08-09 14:01:58 +0200

    :lipstick: changed navbar

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,9 +5,11 @@ document.addEventListener('DOMContentLoaded', () => {
         new App();
     }
 );
+
 class App {
     constructor() {
         this.handleCommentForm();
+        this.openBurger();
     }
 
     handleCommentForm() {
@@ -30,17 +32,34 @@ class App {
 
             const json = await response.json();
 
-            if (json.code === 'COMMENT_ADDED_SUCCESSFULLY'){
+            if (json.code === 'COMMENT_ADDED_SUCCESSFULLY') {
                 console.log(json.message);
                 const commentList = document.querySelector('#comment-list');
                 const commentCount = document.querySelector('#comment-count');
                 const commentContent = document.querySelector('#comment-content');
                 // commentList.insertAdjacentHTML('beforeend', json.html);
-                                commentList.insertAdjacentHTML('afterbegin', json.html); //todo its not working
+                commentList.insertAdjacentHTML('afterbegin', json.html); //todo its not working
                 // commentList.lastElementChild.scrollIntoView({behavior: 'smooth'});
                 commentCount.innerText = json.numberOfComments;
                 commentContent.value = '';
             }
         });
+    }
+
+    openBurger() {
+        const burgerIcon = $('#burger');
+        const navbarMenu = $('#nav-links');
+
+        burgerIcon.on('click', () => {
+                navbarMenu.toggleClass('is-active');
+            }
+        );
+
+        $(document).on('click', (e) => {
+                if (!$(e.target).closest('#burger').length) {
+                    navbarMenu.removeClass('is-active');
+                }
+            }
+        );
     }
 }
